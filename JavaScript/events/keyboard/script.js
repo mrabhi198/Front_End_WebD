35f3bec1c1014ecf83dd081e9d040eddc218ac7e
Abhinav Alok <abhinavalok318@gmail.com>
2024-02-02 13:51:05 +0530

    keyboard events

---
+++ JavaScript/events/keyboard/script.js
@@ -0,0 +1,31 @@
+function key_event(color){
+    var input = document.getElementById('down');
+    input.style.backgroundColor = color;
+}
+
+function key_event1(color){
+    var input1 = document.getElementById('up');
+    input1.style.backgroundColor = color;
+}
+
+function key_event2(color){
+    var input2 = document.getElementById('press');
+    input2.style.backgroundColor = color;
+}
+
+
+// function changeColor(inputValue) {
+//     var input = document.getElementById('myInput');
+//     input.style.backgroundColor = inputValue;
+// }
+
+
+// function key_event(color) {
+//     var downInput = document.getElementById('down');
+//     var upInput = document.getElementById('up');
+//     var pressInput = document.getElementById('press');
+
+//     downInput.style.backgroundColor = color;
+//     upInput.style.backgroundColor = color;
+//     pressInput.style.backgroundColor = color;
+// }
